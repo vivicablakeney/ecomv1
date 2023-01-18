8fe484b3553c7f4d4f2c5d4cf8cc77824218c7bd
Vivica A. Blakeney <vivica.blakeney@AdminisatorsMBP.attlocal.net>
2023-01-17 23:45:18 -0500

    ecom

---
+++ script.js
@@ -0,0 +1,18 @@
+// const menuToggle = document.querySelector(".menu-toggle");
+// const nav = document.querySelector("nav");
+
+// menuToggle.addEventListener("click", () => {
+//   nav.classList.toggle("active");
+// });
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+white_check_mark
+eyes
+raised_hands
+
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
